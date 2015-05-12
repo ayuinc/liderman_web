432864731e040c0d1c0d779eb2e7689dfc2e7102
joseleerazuri <jose.lee@ayuinc.com>
2015-05-12 11:57:54 -0500

    instalacion de ambiente

---
+++ system/expressionengine/config/database.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'localhost';
+$db['expressionengine']['username'] = 'liderman_web_r';
+$db['expressionengine']['password'] = 'l1d3rm4n_l4b0r4t0r1a';
+$db['expressionengine']['database'] = 'liderman_web_db';
+$db['expressionengine']['dbdriver'] = 'mysqli';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = 'C:\\home\\ee_trainning\\repo_github\\system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
