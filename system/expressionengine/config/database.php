c994e64e7827dc5d28890df8e2b1e442e26bb007
AnaMariaAp <amap.alvarado@gmail.com>
2015-08-18 17:38:28 -0500

    Arreglos front

--- system/expressionengine/config/database.php
+++ system/expressionengine/config/database.php
@@ -16,11 +16,9 @@ $db['expressionengine']['cache_on'] = FALSE;
 $db['expressionengine']['autoinit'] = FALSE;
 $db['expressionengine']['char_set'] = 'utf8';
 $db['expressionengine']['dbcollat'] = 'utf8_general_ci';
-<<<<<<< HEAD
-$db['expressionengine']['cachedir'] = '/home/liderman_web/public_html/system/expressionengine/cache/db_cache/';
-=======
-$db['expressionengine']['cachedir'] = 'C:\xampp\htdocs\liderman_web\system\expressionengine\cache\db_cache';
->>>>>>> b3d3d423ca289c3dbf0f26b5359ad90c66091725
+
+$db['expressionengine']['cachedir'] = 'C:\xampp\htdocs\Lidermanweb\system\expressionengine\cache\db_cache';
+
 
 /* End of file database.php */
 /* Location: ./system/expressionengine/config/database.php */
